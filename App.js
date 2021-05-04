6155a4bbf58a658ea3bceacd1bc4005ffc3413f9
yulianashkop <62882084+yulianashkop@users.noreply.github.com>
2021-05-04 16:35:33 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,17 +4,33 @@ import './App.css';
 import Header from './header';
 import Main from './main';
 import Sidebar from "./sidebar";
-
+import { BrowserRouter as Router, Redirect, Route, Switch, useParams } from "react-router-dom";
+import Post from "./Post Page";
+import AboutMe from "./About Me Page";
+import NewPosts from "./New Post Page";
+import  Login from "./login page";
 
 function App() {
-  return (
-      <div>
-        <Header/>
-        <Main/>
-        <Sidebar/>
+    return (
+        <div>
+            <Router>
+                <div>
+                    <Header/>
+                    <Main/>
+                    <Sidebar/>
 
-      </div>
-  );
+                </div>
+                <Switch>
+
+                    <Route path="/post/:id" component={Post}></Route>
+                    <Route path="/AboutMe" component={AboutMe}></Route>
+                    <Route path="/NewPosts" component={NewPosts}></Route>
+                    <Route path="/Login" component={Login}></Route>
+                    <Route path="/" component={Main}></Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
